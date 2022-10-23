40e15a9db107bd31b18cd021ab2ad96903a25378
Jair Costa <costajj@gmail.com>
2022-10-23 15:23:29 -0400

    feat(Navbar) implemented style

---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {Container, Navbar, Nav, ButtonGroup, Button, NavDropdown } from "react-bootstrap";
+import "./Navbar.scss";
+import logo from "../assets/logo.svg";
+
+const Navigation = () => (
+    <Navbar variant="dark" expand="lg">
+        <container>
+            <Navbar.Brand href="#home">
+                <img
+                src={logo}
+                height="30"
+                className="d-inline-block align-top"
+                alt="Magbank"
+                />
+                </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#cards">Cards</Nav.Link>
+                    <Nav.Link href="#whoweare">Who we are</Nav.Link>
+                    <Nav.Link href="#faq">FAQ</Nav.Link>
+                </Nav>
+            <ButtonGroup aria-label="Basic example">
+                <Button variant="outline-light">
+                    <NavDropdown title="Access account" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">
+                            Another action
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">
+                            Separated link
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    </Button>
+                <Button variant="outline-light">Open account</Button>
+            </ButtonGroup>
+            </Navbar.Collapse>
+        </container>
+    </Navbar>
+);
+
+// prop é um parametro que todo componente recebe do react que por default nao e exibido, que é um agregador de todos os parametros que passar para o componente dentro do jsx (no caso aqui, o header dentro do App.js)
+
+export default Navigation;
+

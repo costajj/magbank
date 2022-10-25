5199c219249860763d274547d1f2e50da37dc294
Jair Costa <costajj@gmail.com>
2022-10-24 21:35:33 -0400

    feat(hero) implemented styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 import "./App.scss"
 
 const App = () => (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
-  )
+  );
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,13 +5,13 @@ import logo from "../assets/logo.svg";
 
 const Navigation = () => (
     <Navbar variant="dark" expand="lg">
-        <container>
+        <Container>
             <Navbar.Brand href="#home">
                 <img
                 src={logo}
                 height="30"
-                className="d-inline-block align-top"
-                alt="Magbank"
+                className='d-inline-block align-top'
+                alt='Magbank logo'
                 />
                 </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -24,21 +24,19 @@ const Navigation = () => (
             <ButtonGroup aria-label="Basic example">
                 <Button variant="outline-light">
                     <NavDropdown title="Access account" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">
-                            Another action
+                        <NavDropdown.Item href="#action/3.1">
+                            Person
                         </NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                         <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">
-                            Separated link
+                        <NavDropdown.Item href="#action/3.2">
+                            Entreprise
                         </NavDropdown.Item>
                     </NavDropdown>
                     </Button>
                 <Button variant="outline-light">Open account</Button>
             </ButtonGroup>
             </Navbar.Collapse>
-        </container>
+        </Container>
     </Navbar>
 );
 

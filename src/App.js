c0297ac18ce95b5f664dc630c4ccb2cd065a6fb0
Jair Costa <costajj@gmail.com>
2022-10-30 12:25:35 -0400

    feat(hero) implemented main page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import "./App.scss"
+import CreditCard from "./components/CreditCard";
+import CardList from "./components/CardList";
+import CenteredButton from "./components/CenteredButton";
+import Institutional from './components/Institutional';
+import "./App.scss";
+
+import posts from "./data/posts";
 
 const App = () => (
     <div className="App">
       <Navbar />
       <Hero />
+
+      <CreditCard />
+      <CardList posts={posts} />
+      <CenteredButton>Open Account</CenteredButton>
+      <Institutional />
     </div>
   );
 
